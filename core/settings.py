3e304b16101be91b2fb46d17c18dd5cc6df98cc3
engalisonsantos <eng.alison.santos@gmail.com>
2026-03-02 08:35:51 -0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -1,3 +1,8 @@
+import os
+from pathlib import Path
+import dj_database_url
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 # ===============================
 # DATABASE
 # ===============================
